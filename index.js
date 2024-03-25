138103dceb32a97380d7725cd3548df52f233439
gideon rotich <gideonrotich2001@gmail.com>
2024-03-25 20:09:49 +0300

    complete assingment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+
+//const cat = [
+ // cats.length =0
+  
+  //  cats.push("Milo", "Otis", "Garfield");
+//]
+function [
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+];
