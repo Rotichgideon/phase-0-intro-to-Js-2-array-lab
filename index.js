d538933f8848da93cd74c9e8a4376d26f27c261e
gideon rotich <gideonrotich2001@gmail.com>
2024-04-09 22:19:06 +0300

    complete

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 // Write your solution here!
 
-//const cat = [
- // cats.length =0
-  
-  //  cats.push("Milo", "Otis", "Garfield");
-//]
-function [
-    cats.length = 0;
-  
-    cats.push("Milo", "Otis", "Garfield");
-];
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat (name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat (name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat (name) {
+    cats.shift(name);
+}
+
+function appendCat(name){
+    const newCats = [...cats, name]
+
+    return newCats
+}
+
+function prependCat(name){
+    const newCats = [name, ...cats]
+
+    return newCats
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length-1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
